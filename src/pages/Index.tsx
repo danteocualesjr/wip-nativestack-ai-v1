fcda7aa94605646b3be7080f6e0ccedb47528649
Your Name <danteocualesjr@gmail.com>
2025-03-11 12:43:50 +0800

    updated index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -5,6 +5,7 @@ import Footer from "@/components/Footer";
 import { Link } from "react-router-dom";
 
 const Index = () => {
+  
   const [activeAccordion, setActiveAccordion] = useState<number | null>(null);
 
   return (
