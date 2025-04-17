3635b9d9d49162830f9dfce834cafba95b1b8593
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2025-04-17 20:56:03 +0800

    small refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,9 @@ import AboutUs from "./pages/AboutUs";
 import OurServices from "./pages/OurServices";
 
 import OurWork from "./pages/OurWork";
+
 import Pricing from "./pages/Pricing";
+
 import Contact from "./pages/Contact";
 import Blog from "./pages/Blog";
 import Careers from "./pages/Careers";
