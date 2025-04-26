225f7cd0dc3face60c49ba5ef1032ddc0ed5155d
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2025-04-26 18:00:08 +0800

    small refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, useLocation, useNavigationType } from "react-router-dom";
-
 import { useEffect } from "react";
 
 import Index from "./pages/Index";
