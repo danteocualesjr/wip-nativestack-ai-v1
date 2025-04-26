3dc3f958d9587b14ac9bd17325acb1c47166858e
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2025-04-26 18:00:15 +0800

    small refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, useLocation, useNavigationType } from "react-router-dom";
 import { useEffect } from "react";
-
 import Index from "./pages/Index";
 
 import AboutUs from "./pages/AboutUs";
